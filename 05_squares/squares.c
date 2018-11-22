220dac034306f8efebfb3fc884eecd27e3091aaf
Coursera Learner <nobody@nowhere.nul>
2018-11-22 02:53:30 +0000

    Did assignment 05

--- 05_squares/squares.c
+++ 05_squares/squares.c
@@ -1,6 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int max (int num1, int num2) {
+  if (num1 > num2) {
+    return num1;
+  }
+  else {
+    return num2;
+  }
+}
+
+int rangeCheck (int index, int offset, int size) {
+  if ((index >= offset) && (index < (offset + size))) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+int equalityCheck (int index, int offset, int size) {
+  if ((index == offset) || (index == (offset + size - 1))) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+int eitherOrCheck (int index1, int index2, int size) {
+  if ((index1 < size && (index2 == 0 || index2 == size-1)) || (index2 < size && (index1 == 0 || index1 == size - 1))) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
 
 void squares(int size1, int x_offset, int y_offset, int size2) {
   //compute the max of size1 and (x_offset + size2).  Call this w
@@ -30,4 +62,20 @@ void squares(int size1, int x_offset, int y_offset, int size2) {
     //when you finish counting x from 0 to w, 
     //print a newline
 
+  int w = max(size1, (x_offset + size2));
+  int h = max(size1, (y_offset + size2));
+  for (int y = 0; y < h; y++) {
+    for (int x = 0; x < w; x++) {
+      if (((rangeCheck(x, x_offset, size2) == 1) && (equalityCheck(y, y_offset, size2) == 1)) || ((rangeCheck(y, y_offset, size2) == 1) && (equalityCheck(x, x_offset, size2) == 1))) {
+        printf("*");
+      } else {
+        if (eitherOrCheck(x, y, size1) == 1) {
+          printf("#");
+        } else {
+          printf(" ");
+        }
+      }
+    }
+    printf("\n");
+  }
 }
